89ab35e8dbee329e026a48543d4b11a02018674f
Ville Takanen <vitkukissa@gmail.com>
2021-12-17 14:17:43 +0200

    feat: dark mode toggle

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import '../styles/app.sass'
+  function toggle() {
+    window.document.body.classList.toggle('darkmode')
+  }
 </script>
 
 <slot />
+
+<button on:click={toggle}>Toggle dark mode</button>
