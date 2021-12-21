f21253eabd1ad1aec851ac0dac1265462b399ec3
Ville Takanen <vitkukissa@gmail.com>
2021-12-21 12:30:01 +0200

    fix: login screen alpha

---
+++ src/locales/fi.ts
@@ -0,0 +1,5 @@
+export const fi = {
+  "app": {
+    "title": "Charna"
+  }
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,22 @@
+<script lang="ts">
+  import { signInWithRedirect, GoogleAuthProvider } from 'firebase/auth'
+  import { firebaseAuth } from '../firebase'
+
+  let loggingIn = false
+
+  async function signInWithGoogle() {
+    if (loggingIn) return
+    loggingIn = true
+
+    await signInWithRedirect($firebaseAuth, new GoogleAuthProvider())
+
+    loggingIn = false
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p><a href="/stylebook">Stylebook</a>.</p>
+{#if !loggingIn}
+  <button on:click={signInWithGoogle}>Sign in with Google</button>
+{/if}
